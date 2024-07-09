645d3d7bd5e2bc79494c29da5548b0f81df80fad
OudDriver <80905197+OudDriver@users.noreply.github.com>
2024-07-09 20:09:10 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+from modelRunner import get_class
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+  print(f'We have logged in as {bot.user}')
+
+@bot.command(name="hello")
+async def hello(ctx):
+  await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command(name="check") 
+async def check(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            await attachment.save(f"./{file_name}")
+            await ctx.send(get_class(model_path="./keras_model.h5", labels_path="labels.txt", image_path=f"./{attachment.filename}"))
+    else:
+        await ctx.send("You forgot to upload the image :(")
+
+
+bot.run("MTExMzQyMTY0MzE1OTcwNzY1OA.GSKEQn.SO3guEjso_btfZIQMe4DGP3e2aFVUXWmDT-oxY")
